29b4a7e0e2f7177743c766ccc88d258893be6745
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-12-15 11:41:05 -0600

    Added function for axios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 5,
+    cursor: "",
+  });
+}
 function generateRecipe(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let apiKey = "8f6148bf7t4049b3979ae80dba7bo608";
-    let prompt = "";
-    let context = "";
-    let apiUrl =
-      `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-    
-    new Typewriter("#recipe", {
-      strings: "Recipe",
-      autoStart: true,
-      delay: 5,
-      cursor: "",
-    });
+  let apiKey = "8f6148bf7t4049b3979ae80dba7bo608";
+  let prompt = "";
+  let context = "";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
