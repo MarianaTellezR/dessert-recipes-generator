8ee0392235ab766ace95f03c95897b1ba35a48cc
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-12-15 11:38:44 -0600

    Added API call

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 function generateRecipe(event) {
     event.preventDefault();
+
+    let apiKey = "8f6148bf7t4049b3979ae80dba7bo608";
+    let prompt = "";
+    let context = "";
+    let apiUrl =
+      `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
     new Typewriter("#recipe", {
       strings: "Recipe",
       autoStart: true,
-      delay: 56,
+      delay: 5,
       cursor: "",
     });
 }
