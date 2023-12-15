5149eddb74e802ac9f01a528c5f2a12d936f733b
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-12-15 12:08:56 -0600

    Added AI parameters

--- src/index.js
+++ src/index.js
@@ -11,12 +11,20 @@ function displayRecipe(response) {
 function generateRecipe(event) {
   event.preventDefault();
 
+  let instructionsInput = document.querySelector("#user-instructions").value;
+
   let apiKey = "8f6148bf7t4049b3979ae80dba7bo608";
-  let prompt = "";
-  let context = "";
+
+  let context =
+    "You are an expert chef on desserts and love to create delicious desserts and its recipes. Your mission is to create a recipe of a dessert, including ingredients and steps on how to make the dessert in basic HTML, with the ingredient that the user tells you. Make sure you follow the user instructions. Include a nice title with an h2 from html for the recipe.";
+
+  let prompt = `User instructions: Generate a recipe of a dessert using the ingredient: ${instructionsInput}`;
+
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating dessert recipe");
+  console.log(`Prompt: ${prompt} `);
+  console.log(`Context: ${context} `);
 
   axios.get(apiUrl).then(displayRecipe);
 }
