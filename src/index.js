4e0325e3edeba9853780a9c7fc57d37e8a932488
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-12-21 12:25:25 -0600

    Updated context ai

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generateRecipe(event) {
   let apiKey = "8f6148bf7t4049b3979ae80dba7bo608";
 
   let context =
-    "You are an expert chef on desserts and love to create delicious desserts and its recipes. Your mission is to create a recipe of a dessert, including a description paragraph of the dessert, ingredients and steps on how to make the dessert in basic HTML, with the ingredient that the user tells you. Make sure you follow the user instructions. Include a title for the recipe with h2 and align it to the left. The titles for the steps and ingredients make them h3 Add a horizontal line below the title.";
+    "You are an expert chef on desserts and love to create delicious desserts and its recipes. Your mission is to create a recipe of a dessert, including a description paragraph of the dessert, ingredients and steps on how to make the dessert in basic HTML, with the ingredient that the user tells you. Make sure you follow the user instructions. Include a title for the recipe with h2 and align it to the left. The titles for the steps and ingredients make them h3. Add a horizontal line below the title.";
 
   let prompt = `User instructions: Generate a recipe of a dessert using the ingredient: ${instructionsInput}`;
 
